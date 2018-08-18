e35eaa9d9ebb641be9e1ad99494d03829e3715d1
Rafael <rafa.braun2@gmail.com>
2018-08-17 21:46:28 -0300

    Novos Arquivos Usuario

--- src/dominio/Usuario.java
+++ src/dominio/Usuario.java
@@ -1,5 +1,38 @@
 package dominio;
 
 public class Usuario {
-
+	
+	Integer id;
+	String nome;
+	String senha;
+	public Usuario(Integer id, String nome, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.senha = senha;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", senha=" + senha + "]";
+	}
+	
+	
 }
