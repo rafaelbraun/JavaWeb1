57c874177598e75bcc14405f328a444a47a6c28c
Rafael <rafa.braun2@gmail.com>
2018-08-17 22:27:32 -0300

    Atualizacao

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,10 +1,35 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Usuario;
+
 public class programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Usuario u = new Usuario(null,"Rafael","1");
+		Usuario u2 = new Usuario(null,"Rafael2","12");
+		Usuario u3 = new Usuario(null,"Rafael3","123");
 
+		System.out.println(u.toString());
+		System.out.println(u2.toString());
+		System.out.println(u3.toString());
+		
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("aulajavaweb");
+		
+		EntityManager em = fabrica.createEntityManager();
+		
+		em.getTransaction().begin();
+			em.persist(u);
+			em.persist(u2);
+			em.persist(u3);
+		em.getTransaction().commit();
+		em.close();
+		fabrica.close();
 	}
 
 }
